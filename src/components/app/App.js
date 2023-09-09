f69bf0d973deed09a66cf4c66b41fadf2284d9a7
Anya <use.anya@gmail.com>
2023-09-09 19:27:34 +0300

    add validation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,8 +6,7 @@ import './App.css';
 const App = () => {
   const [quotes, setQuotes] = useState({});
   const [currentQuote, setCurrentQuote] = useState({name: "USD", value: 0});
-  const [multiplier, setMultiplier] = useState(0);
-  const [checkCorrect, setCheckCorrect] = useState(true);
+  const [multiplier, setMultiplier] = useState({value: 0, correct: true, readOnly: false});
 
   const convertService = new ConvertService();
 
@@ -33,24 +32,33 @@ const App = () => {
 
   function onLoadMultiplier(value){
     if (typeof value === "number" && value>0) {
-      setMultiplier(value);
-      setCheckCorrect(true);
+      setMultiplier({value: value, correct: true});
     } else if (value===0) {
-      setMultiplier(0);
-      setCheckCorrect(true);
-    }
+      setMultiplier({value: 0, correct: true});
+    }    
     else {
-      setMultiplier(0);
-      setCheckCorrect(false);
+      setMultiplier({value: 0, correct: false});
     }
   }
 
   const listQuotes = useMemo(()=>{
-    return Object.keys(quotes).map((currency, i)=>(<option key={i}>{currency.slice(3)}</option>));
+    let currencies = [];
+    try{
+      currencies=Object.keys(quotes).map((currency, i)=>(<option key={i}>{currency.slice(3)}</option>));
+    } catch(e) {
+      setMultiplier({value: 0, correct: true, readOnly: true});
+      return <div className="select currency_error">Error! Try to reload the page.</div>;
+    }
+    return (
+      <select className="select" id="quotes" autoFocus onChange={e=>onLoadCurrentQuote(e.target.value)}>
+          {currencies}
+        </select>
+    );
+    // eslint-disable-next-line
   }, [quotes]);
   
   const total = useMemo(()=>{
-    return (currentQuote.value!==0&&multiplier!==0)?(currentQuote.value*multiplier).toFixed(2):multiplier;
+    return (currentQuote.value!==0&&multiplier.value!==0)?(currentQuote.value*multiplier.value).toFixed(2):multiplier.value;
   }, [currentQuote, multiplier]);
 
   return (
@@ -58,7 +66,7 @@ const App = () => {
       <h1 className='title'>USD Converter</h1>
       <div className="converted_values__block">
       <div className="convert value_from">
-          <div className="currency_value">{multiplier}</div>
+          <div className="currency_value">{multiplier.value}</div>
           <div className="currency_name">USD</div>
         </div>
         <i className="fa-solid fa-arrow-right"></i>
@@ -68,12 +76,10 @@ const App = () => {
         </div>
       </div>
       <div className='converted_settings__block'>
-        <select id="quotes" autoFocus onChange={e=>onLoadCurrentQuote(e.target.value)}>
-          {listQuotes}
-        </select>
+        {listQuotes}
         <div className="input_value">
-          <input type="text" onChange={e=>onLoadMultiplier(+(e.target.value))}/>
-          {checkCorrect?null:<span className="correct">Please, enter a number.</span>}
+          <input type="text" readOnly={multiplier.readOnly} defaultValue={0} onChange={e=>onLoadMultiplier(+(e.target.value))}/>
+          {multiplier.correct?null:<span className="correct">Please, enter a number.</span>}
         </div>
       </div>
     </div>
